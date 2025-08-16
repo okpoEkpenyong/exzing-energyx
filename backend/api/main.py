e9c969116383337abaab431bfec18a9f76eaa983
unknown <okpo.ekpenyong@gmail.com>
2025-08-16 09:41:37 +0100

    more corrs fix

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,24 +3,40 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend.api.routers import emissions, credits, devices, health, items
 from backend.api.database import create_db_and_tables
 from backend.api.routers import metrics
+import os
 
 
 
 app = FastAPI(title="Exzing EnergyX Carbon Intelligence Platform", version="1.0")
 
-origins = [
-    "http://localhost:5173",
-    "localhost:5173",
-    "https://exzing-energyx.onrender.com/",
-    "https://energyx.exzing.com",
+allowOrigins = os.environ.get('API_ALLOW_ORIGINS')
 
-]
+environment = os.environ.get('API_ENVIRONMENT')
+
+def originList():
+    if environment is not None and environment == "develop":
+        print("Allowing requests from any origins. API_ENVIRONMENT=", environment)
+        return ["*"]
+    
+    origins = [
+        "https://portal.azure.com",
+        "https://ms.portal.azure.com",
+        "https://exzing-energyx.onrender.com/",
+        "https://energyx.exzing.com",
+        "https://www.exzing.com",
+    ]
+    
+    if allowOrigins is not None:
+        for origin in allowOrigins.split(","):
+            print("Allowing requests from", origin, ". To change or disable, go to ", Path(__file__))
+            origins.append(origin)
+        
+    return origins
 
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=originList(),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
